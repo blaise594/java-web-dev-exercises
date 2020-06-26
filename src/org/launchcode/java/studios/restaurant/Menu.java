3f204dc74a8bc8846d4770a78a4333ec773df7c5
Daniel Rogers <danielbrogers594@gmail.com>
2020-06-25 21:05:35 -0400

    finished studio 3

---
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios.menuapp;
+import java.util.ArrayList;
+public class Menu {
+    //The app should know when the menu was last updated,
+    // so visitors can see that the restaurant is constantly changing and adding exciting new items
+    private Date lastUpdated;
+
+    //The menu consists of several menu items
+    private ArrayList menuItemsList;
+}
---
+++ src/org/launchcode/java/studios/restaurant/MenuItem.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.menuapp;
+
+public class MenuItem {
+    //Each menu item has a price, description, and category (appetizer, main course, or dessert)
+    private double price;
+    private String description;
+    private String category;
+	//It should be possible to display whether a menu item is new or not
+    private boolean isNew;
+
+}
